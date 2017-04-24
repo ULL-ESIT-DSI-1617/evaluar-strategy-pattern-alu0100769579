aa0dcb14700ebdac5c0a6838f8d3b1a49f0d16ea
Raul Martin <alu0100769579@ull.edu.es>
2017-04-24 11:17:28 +0000

    Arreglando test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 var getArea = require("../shapesarea.js");
 
+require('../triangle.js');
+require('should');
+
 describe("getArea", function() {
   it("must compute the triangle area correctly", function() {
-    var result = getArea('Triangle',  { width: 100, height: 100 });
+    var result = getArea.getArea('Triangle',  { width: 100, height: 100 });
     /* There is a white space between consecutive columns */
     result.should.match(/^5000$/);
   })
